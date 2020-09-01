c63e0c7464cf5d292cc7d0cbd92cdf8fa28dd6ae
Aaron Goen <aarongoen2.0@gmail.com>
2020-09-01 17:51:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,8 +28,8 @@ class Song
   
   def self.new_by_filename(file)
     artist = file.split(" - ")[0]
-    name = file.split(" - ")[1]
-    song = self.new(name) 
+    song_name = file.split(" - ")[1]
+    song = self.new(song_name) 
     song.artist = artist 
   end
   
