9452ed7afe0c688a883369d644493fb9c6b38aa5
Aaron Goen <aarongoen2.0@gmail.com>
2020-09-01 19:49:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
     song = self.new(song_name) 
     artist = Artist.find_or_create_by_name(artist)
     song.artist = artist
-    artist.add_song
+    artist.add_song(song)
     song
   end
   
