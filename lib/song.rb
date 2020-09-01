1031ed67b129ed644a3ebeae50d4c49a13d91c82
Aaron Goen <aarongoen2.0@gmail.com>
2020-09-01 17:35:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,27 +8,48 @@ class Song
   
   def initialize(name)
     @name = name 
+    @@all << self 
   end 
   
   def self.all
     @@all
   end 
   
-  def self.new_by_filename(filename)
-    parsed = filename.split(" - ")
-    song = self.new(parsed[1])
-    artist = Artist.find_or_create_by_name(parsed[0])
-    song.artist = artist
-    artist.add_song(song)
-    song 
+  # def self.new_by_filename(filename)
+  #   parsed = filename.split(" - ")
+  # # binding.pry 
+  #   song_name = parsed[1]
+  #   artist_name = parsed[0]
+  #   song = Song.new(song_name)
+  #   song.artist_name = artist_name
+  #   song 
+  # end
+  
+  
+  def self.new_by_filename(file)
+    artist_name = file.split(" - ")[0]
+    song_name = file.split(" - ")[1]
+    song = Song.new(song_name)
+    # Artist.find(artist_name)  
+    # binding.pry 
+    Artist.add_song(song) 
+    song
+    # song = song.artist_name
+    # song
   end
   
-  def artist_name(name)
-    if artist.name == Artist.find_or_create_by_name
-      Artist.add_song(self.name)
-    else 
-      nil 
-    end 
-  end 
+  # def artist_name=(name)
+  #   if name == artist.find_or_create_by_name
+  #     Artist.add_song(self.name)
+  #   else 
+  #     nil 
+  #   end 
+  # end
+  
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    # self.artist = artist
+    artist.add_song(self)
+  end
   
 end 
